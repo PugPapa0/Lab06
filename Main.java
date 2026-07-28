f717ecea04a64021e9b0ad8b2c3a9fd8981dcbef
Travis Becker <becktrav3@icloud.com>
2026-07-27 17:26:37 -0700

    Lab6

--- Main.java
+++ Main.java
@@ -9,10 +9,19 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("My thoughts on the article are:");
+
 		// Part 2: Invoking/calling method
-		
+		articleThoughts();
 	}
 	
 	// Part 1: Creating method definition
-	
+
+	// method header
+	public static void articleThoughts()
+	{
+		// method body
+		System.out.println("A self-driving car should prioritize protecting human life, including passengers and pedestrians.");
+		System.out.println("The algorith should consider the number of people affected, possible injuries, traffic conditions, and ethical guidelines.");
+		System.out.println("Self-driving car algorithms should be created by engineers, lawmakers, ethicists, and society together.");
+	}
 }
